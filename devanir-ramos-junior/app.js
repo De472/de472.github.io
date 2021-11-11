613ce6c5ea41327852a90a97ebaf8f290c33011d
Devanir <61068102+De472@users.noreply.github.com>
2021-11-11 18:07:00 -0300

    Switch que muda o idioma pra inglês

--- devanir-ramos-junior/app.js
+++ devanir-ramos-junior/app.js
@@ -1,3 +1,31 @@
+
+var lang;
+
+fetch("lang.json")
+.then(response => {
+   return response.json();
+})
+.then(data => lang = data);
+
+function changeLang() {
+  var checkbox = document.getElementById('lang');
+  if (checkbox.checked == true) {
+    aboutTitle.innerHTML = lang[1].aboutTitle;
+    aboutText.innerHTML = lang[1].aboutText;
+    knowledgeTitle.innerHTML = lang[1].knowledgeTitle;
+    knowledgeText.innerHTML = lang[1].knowledgeText;
+    knowledgeList.style.display = 'none';
+    cv.textContent = 'Curriculum';
+  } else {
+    aboutTitle.innerHTML = lang[0].aboutTitle;
+    aboutText.innerHTML = lang[0].aboutText;
+    knowledgeTitle.innerHTML = lang[0].knowledgeTitle;
+    knowledgeText.innerHTML = lang[0].knowledgeText;
+    knowledgeList.style.display = 'none';
+    cv.textContent = 'Baixar CV';
+  }
+};
+
 new Vue({
   el: '#wrapper-footer',
   methods: {
@@ -11,6 +39,7 @@ new Vue({
     }
   }
 });
+
 
 /*
 new Vue({
